640fee0859bfd41cf4b8c52dbd50cec2c9dc0891
Alfredo <70611360+aberlanga-gatech@users.noreply.github.com>
2021-06-30 18:10:47 -0500

    Add files via upload

---
+++ cloud_test/rng_sender/rng_sender.ino
@@ -0,0 +1,112 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+// WiFi connection
+const char *ssid = "ssid_name";
+const char *password = "password";
+
+// MQTT server stuff
+const char *mqtt_server = "driver.cloudmqtt.com";
+const int mqtt_port = 18678;
+const char *mqtt_user = "lvumygpt";
+const char *mqtt_pwd = "JyCVNlyqGHQf";
+const char *mainTopic = "Main_Topic";
+
+// Wifi globals
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+// Message globals
+String toSend;
+char buf[3];
+
+void setup_wifi()
+{
+    delay(10);
+    Serial.println(); // print empty line to demarcate connection attempt
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    WiFi.mode(WIFI_STA);        // define wifi connection security mode
+    WiFi.begin(ssid, password); // connect to wifi
+
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print("."); // print dots every .5 seconds until connected
+    }
+
+    Serial.println("");
+    Serial.println("Connection to WiFi successful");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+void callback(char *topic, byte *payload, unsigned int length)
+{
+    String incoming = "";
+    Serial.print("Topic: "); // println "Topic: [topic]"
+    Serial.println(topic);
+    Serial.print("Message: "); // println "Message: [msg]"
+    for (int i = 0; i < length; i++)
+    {
+        incoming += (char)payload[i];
+    }
+    incoming.trim();
+    Serial.println(incoming);
+    Serial.println();
+
+    String topicStr(topic);
+}
+
+void reconnect()
+{
+    // retries connection to mqtt if still not connected
+    // throw exception if takes too long
+    while (!client.connected())
+    {
+        Serial.println("Attempting to connect to MQTT broker");
+        // generate random client ID to keep trying with different aliases
+        String clientID = "ESP_Client_";
+        clientID += String(random(0xffff), HEX);
+        // attempts do connect
+        if (client.connect(clientID.c_str()))
+        {
+            Serial.println("Connected");
+            // publish connection method to placeholder topic
+            client.publish(mainTopic, "TESTING CONNECTION");
+            client.subscribe("inPHold_Topic");
+        }
+        else
+        {
+            Serial.print("Failed, rc=");
+            Serial.print(client.state());
+            Serial.println("Try again in 5 seconds");
+            delay(5000);
+        }
+    }
+}
+
+void setup()
+{
+    // pins configs
+    pinMode(BUILTIN_LED, OUTPUT);
+    Serial.begin(115200);                // common value for NodeMCU
+    setup_wifi();                        // setup wifi connection (internet link)
+    client.setServer(mqtt_server, 1883); // server address, port
+    client.setCallback(callback);        // ref to callback() func (on_message equiv.)
+}
+
+void loop()
+{
+    if (!client.connected())
+    {
+        reconnect(); // perform reconnect loop
+    }
+    client.loop(); // perform this function on mcu client (a lil recursion) in case of failure
+    toSend = String(random(300));
+    toSend.toCharArray(buf, 3);
+    Serial.print("Publishing message: ");
+    Serial.println(toSend);         // display what message is to be published
+    client.publish(mainTopic, buf); // publish to the placeholder topic
+}

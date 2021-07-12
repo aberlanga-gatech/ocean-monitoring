3164198d517168739645fff71ef24c762c64d58f
aberlanga-gatech <70611360+aberlanga-gatech@users.noreply.github.com>
2021-07-12 11:20:06 -0500

    changed wifi

--- cloud_test/rng_sender/rng_sender.ino
+++ cloud_test/rng_sender/rng_sender.ino
@@ -4,7 +4,7 @@
 
 // WiFi connection
 const char *ssid = "Chato1";
-const char *password = "ChatoBubu";
+const char *password = "ChatoBubu1";
 
 // MQTT server stuff
 const char *mqtt_server = "driver.cloudmqtt.com";
@@ -86,7 +86,7 @@ void reconnect()
         Serial.println("Attempting to connect to MQTT broker");
 
         String clientID = "ESP_Client_";
-        clientID += String(random(0xffff), HEX);
+        clientID += 6;  //String(random(0xffff), HEX);
 
         if (client.connect(clientID.c_str(), mqtt_user, mqtt_pass))
         {

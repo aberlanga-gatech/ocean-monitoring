355b7fb8e88aa9281fde2945d88d60b7f9ce99f9
aberlanga-gatech <70611360+aberlanga-gatech@users.noreply.github.com>
2021-07-23 13:41:41 -0500

    typio

--- backend/handler.py
+++ backend/handler.py
@@ -40,7 +40,7 @@ def on_message(client, userdata, msg):
 
     #save to db
     dbrow = (payload['device_id'], payload['temperature'], payload['pressure'], payload['ph'], payload['oxygen'],payload['lat'],payload['lng'])
-    cur_insert.execute("INSERT INTO telemetry(time,device_id,temperature,pressure,ph,o2,lat,lng) VALUES (NOW(),%s,%s,%s,%s,%s,%s,%s)", dbrow)
+    cur_insert.execute("INSERT INTO telemetry(time,device_id,temperature,pressure,ph,oxygen,lat,lng) VALUES (NOW(),%s,%s,%s,%s,%s,%s,%s)", dbrow)
     conn.commit()
 
 #client configuration
